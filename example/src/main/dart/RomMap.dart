cdbd34ec0aabe5786457f2cc029dce48e981793b
dzwicker <daniel.zwicker@in2experience.com>
2012-11-29 15:46:51 +0100

    add pub dependency to example project

--- example/src/main/dart/RomMap.dart
+++ example/src/main/dart/RomMap.dart
@@ -1,8 +1,19 @@
+import "package:log4dart/log4dart.dart";
 
 void main() {
   print("Dart geht!");
+  new RomMap().init();
 }
 
 class RomMap {
-  
+
+  final Logger _logger;
+
+  RomMap() : _logger = LoggerFactory.getLogger("RomMap");
+
+  void init() {
+    _logger.info("a info message");
+
+    _logger.warnFormat("%s %s", ["a warning message", "formated using c's sprintf syntax"]);
+  }
 }
