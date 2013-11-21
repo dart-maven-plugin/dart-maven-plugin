9cce22dbd881fbd498acc6ae2d0dea4d153c847e
dzwicker <daniel.zwicker@in2experience.com>
2013-11-21 21:09:13 +0100

    the pub goal know need the command to be executed and options to be passed to this command

--- src/main/java/com/google/dart/PubMojo.java
+++ src/main/java/com/google/dart/PubMojo.java
@@ -1,9 +1,6 @@
 package com.google.dart;
 
-import java.io.File;
-import java.io.OutputStreamWriter;
-import java.util.Set;
-
+import com.google.dart.util.OsUtil;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -14,7 +11,10 @@ import org.codehaus.plexus.util.cli.Commandline;
 import org.codehaus.plexus.util.cli.StreamConsumer;
 import org.codehaus.plexus.util.cli.WriterStreamConsumer;
 
-import com.google.dart.util.OsUtil;
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Goal to invoke the dart pub package manager.
@@ -24,90 +24,100 @@ import com.google.dart.util.OsUtil;
 @Mojo(name = "pub")
 public class PubMojo extends AbstractDartMojo {
 
-	private final static String COMMAND_INSTALL = "install";
+    /**
+     * The command pub should execute.
+     *
+     * @since 3.0.0
+     */
+    @Parameter(defaultValue = "get", property = "dart.pub.command")
+    private String pubCommand;
 
-	private final static String COMMAND_UPDATE = "update";
+    /**
+     * Options to be passed to the pub command.
+     *
+     * @since 3.0.0
+     */
+    @Parameter
+    private List<String> pubOptions;
 
-	/**
-	 * Skip the execution of dart2js.
-	 *
-	 * @since 1.0.3
-	 */
-	@Parameter(defaultValue = "false", property = "dart.pub.update")
-	private boolean update;
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        final Set<File> dartPackageRoots = findDartPackageRoots();
+        processPubDependencies(dartPackageRoots);
+    }
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		final Set<File> dartPackageRoots = findDartPackageRoots();
-		processPubDependencies(dartPackageRoots);
-	}
+    protected void processPubDependencies(final Set<File> dartPackageRoots) throws MojoExecutionException {
 
-	protected void processPubDependencies(final Set<File> dartPackageRoots) throws MojoExecutionException {
+        if (isPubSkipped()) {
+            getLog().info("Updating dependencies (pub packagemanager) is skipped.");
+            return;
+        }
+        String pubPath;
+        checkPub();
+        pubPath = getPubExecutable().getAbsolutePath();
 
-		if (isPubSkipped()) {
-			getLog().info("Updating dependencies (pub packagemanager) is skipped.");
-			return;
-		}
-		String pubPath = null;
-		checkPub();
-		pubPath = getPubExecutable().getAbsolutePath();
+        if (getLog().isDebugEnabled()) {
+            getLog().debug("Using pub '" + pubPath + "'.");
+            getLog().debug("basedir: " + getBasedir());
+        }
 
-		if (getLog().isDebugEnabled()) {
-			getLog().debug("Using pub '" + pubPath + "'.");
-			getLog().debug("basedir: " + getBasedir());
-		}
+        final StreamConsumer output = new WriterStreamConsumer(new OutputStreamWriter(System.out));
+        final StreamConsumer error = new WriterStreamConsumer(new OutputStreamWriter(System.err));
 
-		final StreamConsumer output = new WriterStreamConsumer(new OutputStreamWriter(System.out));
-		final StreamConsumer error = new WriterStreamConsumer(new OutputStreamWriter(System.err));
+        final Commandline cl = new Commandline();
+        cl.setExecutable(pubPath);
 
-		final Commandline cl = new Commandline();
-		cl.setExecutable(pubPath);
+        cl.createArg().setValue(pubCommand);
 
-		cl.createArg().setValue(update ? COMMAND_UPDATE : COMMAND_INSTALL);
+        if (pubOptions != null) {
+            for (final String option : pubOptions) {
+                cl.createArg().setValue(option);
+            }
+        }
 
-		if (getLog().isDebugEnabled()) {
-			getLog().debug("Base pub command: " + cl.toString());
-		}
+        if (getLog().isDebugEnabled()) {
+            getLog().debug("Base pub command: " + cl.toString());
+        }
 
-		System.out.println();
-		System.out.println();
+        System.out.println();
+        System.out.println();
 
-		try {
-			for (final File dartPackageRoot : dartPackageRoots) {
-				getLog().info("Run pub for package root: " + relativePath(dartPackageRoot));
-				cl.setWorkingDirectory(dartPackageRoot);
-				if (getLog().isDebugEnabled()) {
-					getLog().debug("Execute pub command: " + cl.toString());
-				}
-				final int returnCode = CommandLineUtils.executeCommandLine(cl, output, error);
-				if (getLog().isDebugEnabled()) {
-					getLog().debug("pub return code: " + returnCode);
-				}
-				if (returnCode != 0) {
-					throw new MojoExecutionException("Pub returned error code " + returnCode);
-				}
-			}
-		} catch (CommandLineException e) {
-			throw new MojoExecutionException("Unable to execute pub", e);
-		}
+        try {
+            for (final File dartPackageRoot : dartPackageRoots) {
+                getLog().info("Run pub for package root: " + relativePath(dartPackageRoot));
+                cl.setWorkingDirectory(dartPackageRoot);
+                if (getLog().isDebugEnabled()) {
+                    getLog().debug("Execute pub command: " + cl.toString());
+                }
+                final int returnCode = CommandLineUtils.executeCommandLine(cl, output, error);
+                if (getLog().isDebugEnabled()) {
+                    getLog().debug("pub return code: " + returnCode);
+                }
+                if (returnCode != 0) {
+                    throw new MojoExecutionException("Pub returned error code " + returnCode);
+                }
+            }
+        } catch (CommandLineException e) {
+            throw new MojoExecutionException("Unable to execute pub", e);
+        }
 
-		System.out.println();
-		System.out.println();
-	}
+        System.out.println();
+        System.out.println();
+    }
 
-	protected void checkPub() throws MojoExecutionException {
-		checkDartSdk();
-		if (!getPubExecutable().canExecute()) {
-			throw new MojoExecutionException("Pub not executable! Configuration error for dartSdk? dartSdk="
-					+ getDartSdk().getAbsolutePath());
-		}
-	}
+    protected void checkPub() throws MojoExecutionException {
+        checkDartSdk();
+        if (!getPubExecutable().canExecute()) {
+            throw new MojoExecutionException("Pub not executable! Configuration error for dartSdk? dartSdk="
+                + getDartSdk().getAbsolutePath());
+        }
+    }
 
-	private File getPubExecutable() {
-		return new File(getDartSdk(), "bin/pub" + (OsUtil.isWindows() ? ".bat" : ""));
-	}
+    private File getPubExecutable() {
+        return new File(getDartSdk(), "bin/pub" + (OsUtil.isWindows() ? ".bat" : ""));
+    }
 
-	public boolean isPubSkipped() {
-		return false;
-	}
+    public boolean isPubSkipped() {
+        return false;
+    }
 }
